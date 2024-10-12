9e39dfef19811a162cb0d657760e83fbfccc063f
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-12 20:50:01 +0300

    redux

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,11 +1,86 @@
+import Psychology from "./Images/Psychology.jpeg";
+import ScienceFiction from "./Images/ScienceFiction.jpeg";
+import Novel from "./Images/Novel.jpeg";
+import Comics from "./Images/Comics.jpeg";
+import Poem from "./Images/Poem.jpeg";
+import Research from "./Images/Research.jpeg";
+import Historical from "./Images/Historical.jpeg";
+import Literature from "./Images/Literature.jpeg";
+import Magazine from "./Images/Magazine.jpeg";
+import Horror from "./Images/Horror.jpeg";
+
 export const Categories = [
     "Psychology",
     "Science Fiction",
     "Novel",
+    "Comics",
     "Poem",
     "Research",
     "Historical",
     "Literature",
-    "Comics",
     "Magazine",
+    "Horror"
+];
+
+export const mockData = [
+    {
+        id: 1,
+        image: Psychology,
+        name: "Product 1",
+        price: 10
+    },
+    {
+        id: 2,
+        image: ScienceFiction,
+        name: "Product 2",
+        price: 10
+    },
+    {
+        id: 3,
+        image: Novel,
+        name: "Product 3",
+        price: 10
+    },
+    {
+        id: 4,
+        image: Comics,
+        name: "Product 4",
+        price: 10
+    },
+    {
+        id: 5,
+        image: Poem,
+        name: "Product 5",
+        price: 10
+    },
+    {
+        id: 6,
+        image: Research,
+        name: "Product 6",
+        price: 10
+    },
+    {
+        id: 7,
+        image: Historical,
+        name: "Product 7",
+        price: 10
+    },
+    {
+        id: 8,
+        image: Literature,
+        name: "Product 8",
+        price: 10
+    },
+    {
+        id: 9,
+        image: Magazine,
+        name: "Product 9",
+        price: 10
+    },
+    {
+        id: 10,
+        image: Horror,
+        name: "Product 10",
+        price: 10
+    },
 ];
