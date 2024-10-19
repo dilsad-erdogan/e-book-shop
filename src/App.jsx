7d46c59266b572a392a7789c277363e3a42f03eb
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-19 19:59:12 +0300

    order

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Order from "./pages/Order";
 
 function App() {
+  const [order, setOrder] = useState(null);
+
   return (
     <BrowserRouter>
       <Navbar />
@@ -14,6 +19,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/checkout" element={<Checkout setOrder={setOrder} />}></Route>
+        <Route path="/order-confirmation" element={<Order order={order} />}></Route>
       </Routes>
 
       <Footer />
